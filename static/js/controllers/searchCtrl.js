863b274615622964283d68e2d0b3d4146d08f183
tjespe <tordjohanespe@gmail.com>
2017-08-27 20:23:49 +0200

    Removed unnecessary code

--- static/js/controllers/searchCtrl.js
+++ static/js/controllers/searchCtrl.js
@@ -7,10 +7,8 @@ app.controller('searchController', ['$http', '$scope', '$q', 'domain', function(
   vm.search = function () {
     $http.get('/static/php/search.php?q='+vm.query).success(function (data) {
       vm.matches = data[1];
-      console.log("Yr svarte, respons:", data);
-      console.log("vm.matches nå:", vm.matches);
     }).error(function (data,status) {
-      console.log("Feil:", data, status);
+      console.warn("Feil:", data, status);
     });
   }
 
