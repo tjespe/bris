adc1d155dc948237dc8833ac266bb8fb48569a72
tjespe <tordjohanespe@gmail.com>
2017-09-01 10:19:38 +0200

    Created a new search mechanism

--- static/js/controllers/searchCtrl.js
+++ static/js/controllers/searchCtrl.js
@@ -1,15 +1,28 @@
-app.controller('searchController', ['$http', '$scope', '$q', 'domain', function($http, $scope, $q, domain) {
+app.controller('searchController', ['$http', '$scope', '$q', 'domain', '$sce', function($http, $scope, $q, domain, $sce) {
   var vm = this;
   vm.query = "";
   vm.matches = [];
   vm.error = false;
 
   vm.search = function () {
-    $http.get('/static/php/search.php?q='+vm.query).success(function (data) {
-      vm.matches = data[1];
-    }).error(function (data,status) {
-      console.warn("Feil:", data, status);
+    $http.get("https://real-timer-server.tk:2087/bris.php?input="+vm.query).success((data)=>{
+      console.log(data);
+      vm.matches = data.predictions;
+      for (let i = 0;i < vm.matches.length;i++) {
+        // This will produce a correct URL for most countries, if any exceptions are discovered, find the correct URL here: https://raw.githubusercontent.com/divyabiyani/Countries-Flag-API/master/Countries.json
+        let country = vm.matches[i].terms[vm.matches[i].terms.length - 1].value;
+        if (country == "United States") country = "usa";
+        if (country == "Iran") country = "irn";
+        if (country == "United Kingdom") country = "gbr";
+        if (country == "Germany") country = "deu";
+        if (country == "Spain") country = "esp";
+        if (country == "Malaysia") country = "mys";
+        if (country == "Malta") country = "mlt";
+        if (country == "United Arab Emirates") country = "are";
+        if (country == "Japan") country = "jpn";
+        if (country == "Denmark") country = "dnk";
+        vm.matches[i].country = country.substr(0,3).toLowerCase();
+      }
     });
   }
-
 }]);
--- static/views/search.php
+++ static/views/search.php
@@ -19,13 +19,13 @@ if($norsk){
   <div ng-controller="searchController as s">
     <input type="text" ng-model="s.query" placeholder="<?= $text->{'typeLocation'} ?>" ng-keyup="s.search()">
     <div ng-if="s.matches.length>0" class="matching-locations">
-      <a ng-repeat="match in s.matches" class="row location-row" href="{{match[1].replace('/sted', '')}}">
+      <a ng-repeat="match in s.matches" class="row location-row" href="{{'/'+match.description}}">
         <div class="place-description">
-          {{match[0]}}
-          <div>{{match[2]}}</div>
+          <span ng-bind="match.structured_formatting.main_text"></span>
+          <div ng-bind="match.structured_formatting.secondary_text"></div>
         </div>
         <div>
-          <img ng-src="{{'https://fil.nrk.no/contentfile/web/icons/flags/h14/'+match[3]+'.png'}}" alt="" />
+          <img ng-src="{{'https://restcountries.eu/data/'+match.country+'.svg'}}" alt="" />
         </div>
       </a>
     </div>
