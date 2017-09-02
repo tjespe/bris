c18d8e596fcfa5b64d04262273724be160815a2d
tjespe <tordjohanespe@gmail.com>
2017-09-02 19:39:53 +0200

    Removed PHP code from views/search.php

--- static/views/search.php
+++ static/views/search.php
@@ -1,22 +1,6 @@
-<?php
-
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT');
-header('Content-Type: application/json;charset=utf-8');
-$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-$norsk = ($lang == "nb" || $lang == "nn" || $lang == "no" || (isset($_GET['lang']) && substr($_GET['lang'], 0, 2) == "no")) && !(isset($_GET['lang']) && substr($_GET['lang'], 0, 2) == "en");
-if($norsk){
-  $text = json_decode(file_get_contents("../js/objects/no-text.json"));
-} else {
-  $text = json_decode(file_get_contents("../js/objects/en-text.json"));
-}
-
- ?>
-
 <div ng-if="master.lazyModulesLoaded">
   <div ng-controller="searchController as s">
-    <input type="text" ng-model="s.query" placeholder="<?= $text->{'typeLocation'} ?>" ng-keyup="s.search()">
+    <input type="text" ng-model="s.query" placeholder="{{master.textData.typeLocation}}" ng-keyup="s.search()">
     <div ng-if="s.matches.length>0" class="matching-locations">
       <a ng-repeat="match in s.matches" class="row location-row" href="{{'/'+match.description}}">
         <div class="place-description">
@@ -28,8 +12,6 @@ if($norsk){
         </div>
       </a>
     </div>
-    <div ng-if="s.matches.length == 0 && s.query.length > 0">
-      <?= $text->{'noMatches'} ?>
-    </div>
+    <div ng-if="s.matches.length == 0 && s.query.length > 0" ng-bind="master.textData.noMatches"></div>
   </div>
 </div>
