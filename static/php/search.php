9a3560c9df9000f406e625ecc15c826b83910f06
tjespe <tordjohanespe@gmail.com>
2017-09-02 17:25:03 +0200

    Moved PHP script to external server

--- static/php/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT');
-header('Content-Type: application/json;charset=utf-8');
-echo file_get_contents('http://www.yr.no/_/websvc/jsonforslagsboks.aspx?s='.urlencode($_GET['q']));
-
- ?>
