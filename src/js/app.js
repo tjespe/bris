04d6461144f9a35a6300ec33e28842a8537296ab
tjespe <tordjohanespe@gmail.com>
2017-09-04 17:07:54 +0200

    Removed unused code

--- src/js/app.js
+++ src/js/app.js
@@ -17,40 +17,10 @@ app.config(["$routeProvider", "$sceProvider", "$locationProvider", '$controllerP
 
   $locationProvider.html5Mode(true);
 
-  // Activate lazy-loading of modules:
+  // Activate lazy-loading of controllers:
   app._controller = app.controller;
-  //app._service = app.service;
-  //app._factory = app.factory;
-  //app._value = app.value;
-  //app._directive = app.directive;
-  // Provider-based controller.
   app.controller = function( name, constructor ) {
     $controllerProvider.register( name, constructor );
     return( this );
   };
-  // Provider-based service.
-  /*app.service = function( name, constructor ) {
-    $provide.service( name, constructor );
-    return( this );
-  };*/
-  // Provider-based factory.
-  /*app.factory = function( name, factory ) {
-    $provide.factory( name, factory );
-    return( this );
-  };*/
-  // Provider-based value.
-  /*app.value = function( name, value ) {
-    $provide.value( name, value );
-    return( this );
-  };*/
-  // Provider-based directive.
-  /*app.directive = function( name, factory ) {
-    $compileProvider.directive( name, factory );
-    return( this );
-  };*/
-  // Provider-based filter
-  /*app.filter = function (name, constructor) {
-    $filterProvider.register(name, constructor);
-    return( this );
-  };*/
 }]);
